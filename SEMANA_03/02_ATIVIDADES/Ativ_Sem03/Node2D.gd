afb1806480202042181a436cd0393f797609dceb
Ana Luisa Goes <ana.barbosa@sou.inteli.edu.br>
2023-02-17 21:45:50 -0300

    Add comentario ao codigo
    adicionando comentário ao código para facilitação de compreensão

--- SEMANA_03/02_ATIVIDADES/Ativ_Sem03/Node2D.gd
+++ SEMANA_03/02_ATIVIDADES/Ativ_Sem03/Node2D.gd
@@ -1,40 +1,42 @@
 extends Node2D
 
 #variaveis para atribuir texto
-var lista_user
-var lista
-var resp
+var lista_user #variavel do usuario
+var lista #variavel pronta
+var resp 
 
-var texto_user
-var texto
+#adicionar texto 
+var texto_user #variavel do usuario
+var texto #variavel pronta
 
 
 func _ready():
 	pass
 
 
-# LISTA DO USUARIO
-func _on_lista_user_pressed():
-	lista_user = str($dado1.text)
-	resp = str(lista_user.split(' '))
+# LISTA QUE O USUARIO DIGITA
+func _on_lista_user_pressed(): #Quando pressionar o botão 
+	lista_user = str($dado1.text) #Atribuindo texto do dado no quadro branco
+	resp = str(lista_user.split(' ')) #funcao split() -> separar texto com virgula
 	
-	$ColorRect/lab3.text = "Sua lista: " + resp 
+	$ColorRect/lab3.text = "Sua lista: " + resp #adicionando texto ao quadro de saida
 
 
 
-# LISTA COM RESULTADO
+# LISTA COM RESULTADO PRONTO 
 func _on_lista_pressed():
-	$ColorRect/lab3.text = "Lista: [12, 11, 10, 9]"
+	$ColorRect/lab3.text = "Lista: [12, 11, 10, 9]" #lista pronta
 
 
 
-# TEXTO DO USUARIO
+# TEXTO QUE USUARIO DIGITA
 func _on_text_user_pressed():
-	texto_user = $dado2.text
+	texto_user = $dado2.text #atribuindo texto digitado pelo usuario
 	
-	$ColorRect/lab3.text = " " + texto_user
+	$ColorRect/lab3.text = " " + texto_user #atribuindo ao quadro de saida
 	
-# TEXTO COM RESULTADO
+	
+# TEXTO COM RESULTADO PRONTO
 func _on_text_pressed():
 	texto = $texto.text
 	
